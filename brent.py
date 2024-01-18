e16ecdffbef8d1ceb16fe5e491fc0ef6615faeed
Brent Philion <brent@philiontech.com>
2024-01-18 18:22:52 -0500

    csv_code

--- brent.py
+++ brent.py
@@ -1,1 +1,33 @@
-pass
+import random
+
+temp = random.randint(-10,35)
+humid = random.randint(10,100)
+
+print(temp)
+print(humid)
+
+csvout = str(f'{temp},{humid} \n')
+print('temp is ' + str(temp) +'C and humidity is ' + str(humid) +'%')
+
+print(csvout)
+
+#append to file Code goes here
+file = open("temp_data.csv" , 'a')
+
+#TO READ THE FILE
+#print(file.read())
+
+
+
+print(file)
+file.write(csvout)
+file.close
+
+file = open("temp_data.csv" , 'r')
+
+#TO READ THE FILE
+print(file.read())
+file.close
+
+#Loop back or wait till the next measurement
+
