ba52595ccafb25062a98bd8a3dc9cbd76092c791
Brent Philion <brent@philiontech.com>
2024-01-18 19:14:46 -0500

    Created file with both pats of our code called both_parts.py

---
+++ both_parts.py
@@ -0,0 +1,86 @@
+'''
+First part reads temp from a DHT connecto to the pi
+
+second part save the data as CSV
+'''
+
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+#record entrys to a list hen a file using a DHT tempetur sensor
+import time
+import board
+import adafruit_dht
+
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D4)
+
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+
+temp_list =[]
+humi_list =[]
+
+while True:
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
+        
+        temp_list.append(temperature_c)
+        humi_list.append(humidity)
+        print(humi_list)
+        print(temp_list)
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(2.0)
+
+#second part  write csv
+import random
+
+temp = random.randint(-10,35)
+humid = random.randint(10,100)
+
+print(temp)
+print(humid)
+
+csvout = str(f'{temp},{humid} \n')
+print('temp is ' + str(temp) +'C and humidity is ' + str(humid) +'%')
+
+print(csvout)
+
+#append to file Code goes here
+file = open("temp_data.csv" , 'a')
+
+#TO READ THE FILE
+#print(file.read())
+
+
+
+print(file)
+file.write(csvout)
+file.close
+
+file = open("temp_data.csv" , 'r')
+
+#TO READ THE FILE
+print(file.read())
+file.close
+
+#Loop back or wait till the next measurement
+
